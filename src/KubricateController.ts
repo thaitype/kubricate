edff59046476c9271a4fde878355f2a54af9dc9b
Thada Wangthammang <thada.wth@gmail.com>
2025-03-08 00:00:22 +0700

    feat: add managed-by label injection in resource configuration

--- src/KubricateController.ts
+++ src/KubricateController.ts
@@ -10,18 +10,35 @@ export type ResourceStore = Record<
   }
 >;
 
+export const LABEL_MANAGED_BY_KEY = 'thaitype.dev/managed-by';
+export const LABEL_MANAGED_BY_VALUE = 'kubricate';
+
 export class KubricateController<Resource extends Record<string, unknown> = {}> {
   resources: ResourceStore = {};
   override: Record<string, unknown> = {};
 
+  private attachLabels(config: Record<string, any>, labels: Record<string, string>) {
+    if (!config.metadata?.labels) {
+      config.metadata.labels = {};
+    }
+    config.metadata.labels = { ...config.metadata?.labels, ...labels };
+    return config;
+  }
+
   build() {
     const result: Record<string, unknown> = {};
     for (const key of Object.keys(this.resources)) {
-      const { type, config } = this.resources[key];
+      const { type } = this.resources[key];
+      let { config } = this.resources[key];
       if (!type) {
         result[key] = config;
         continue;
       }
+      // Inject the managed-by label
+      const injectedLabel: Record<string, string> = {};
+      injectedLabel[LABEL_MANAGED_BY_KEY] = LABEL_MANAGED_BY_VALUE;
+      config = this.attachLabels(config, injectedLabel);
+      // Create the resource
       result[key] = new type(merge({}, config, this.override ? this.override[key] : {}));
     }
     return Object.values(result);
