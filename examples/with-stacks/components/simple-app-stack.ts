daa5239361254a6331698be662b0746c744b01af
Thada Wangthammang <thada.wth@gmail.com>
2025-03-27 13:22:05 +0700

    feat(examples): add new example project with stacks configuration

---
+++ examples/with-stacks/components/simple-app-stack.ts
@@ -0,0 +1,22 @@
+import { SimpleAppStack, NamespaceStack } from "@kubricate/stacks";
+
+const namespace = new NamespaceStack()
+  .configureStack({
+    name: "my-namespace"
+  }).build();
+
+const myApp = new SimpleAppStack()
+  .configureStack({
+    imageName: "nginx",
+    name: "my-app",
+  })
+  .overrideStack({
+    service: {
+      spec: {
+        type: "LoadBalancer"
+      }
+    }
+  })
+  .build();
+
+export default [namespace, myApp];
