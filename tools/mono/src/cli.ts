143fcca4b379f8a337fd060adc8bb44498c9cedf
Thada Wangthammang <thada.wth@gmail.com>
2025-03-27 15:50:10 +0700

    refactor(scripts): update build and dev scripts for improved mono support

--- tools/mono/src/cli.ts
+++ tools/mono/src/cli.ts
@@ -1,15 +1,28 @@
 import { type MonoScripts, processArgs, runCommand } from './libs.js';
 
+const binName = 'mono';
+
 // prettier-ignore
 const scripts: MonoScripts = {
+  // Lint Scripts
   'lint:check': 'eslint src',
   'lint:fix': 'eslint src --fix',
+
+  // Test Scripts
   'test': 'vitest run',
   'test:watch': 'vitest watch',
-  'build': 'esbuild ./src/index.ts --bundle --minify --platform=node --outfile=dist/index.js',
-  'dev': 'tsx watch ./src/index.ts',
-  'start': 'tsx ./src/index.ts',
+
+  // Build in order: ESM, CJS, Annotate for support dual package both ESM and CJS
+  'build': [`${binName} build-esm`, `${binName} build-cjs`, `${binName} build-annotate`],
+  'build-esm': 'tsc',
+  'build-cjs': 'babel dist/esm --plugins @babel/transform-export-namespace-from --plugins @babel/transform-modules-commonjs --out-dir dist/cjs --source-maps',
+  'build-annotate': 'babel dist --plugins annotate-pure-calls --out-dir dist --source-maps',
+  // Compile TypeScript in watch mode
+  'dev': 'tsc -w',
+  // Check TypeScript types
   'check-types': 'tsc --noEmit',
+
+  'start': 'echo "No start script"',
 };
 
 (async () => {
