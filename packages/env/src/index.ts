e410018de0d5927c271ee5678dfe0852267ca26b
Thada Wangthammang <thada.wth@gmail.com>
2025-03-29 21:48:32 +0700

    feat(env): update package structure and remove deprecated shared module

--- packages/env/src/index.ts
+++ packages/env/src/index.ts
@@ -1,1 +1,1 @@
-export * from './lib/shared.js';
+export * from './EnvLoader.js';
--- packages/env/src/lib/shared.spec.ts
+++
@@ -1,8 +0,0 @@
-import { expect, it, describe } from 'vitest';
-import { shared } from './shared.js';
-
-describe('shared', () => {
-  it('should work', () => {
-    expect(shared()).toEqual('shared');
-  });
-});
