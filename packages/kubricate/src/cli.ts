70e1b2f3fefb1c2c0f199fc1856a19c91c3afeb0
Thada Wangthammang <thada.wth@gmail.com>
2025-03-29 11:14:38 +0700

    feat(kubricate): implement ApplyCommand for handling config for loaded stacks

--- packages/kubricate/src/cli.ts
+++ packages/kubricate/src/cli.ts
@@ -2,6 +2,7 @@ import cac from 'cac';
 import c from 'ansis';
 import { readFileSync } from 'node:fs';
 import { join } from 'node:path';
+import { ApplyCommand, ApplyCommandOptions } from './commands/apply.js';
 
 const pkg = {
   version: '0.0.0',
@@ -20,26 +21,8 @@ cli
   .option('--config <config>', 'Config file')
   .option('--outDir <dir>', 'Output directory', { default: '.kubricate' })
   // Action
-  .action(async (options: {
-    root: string;
-    config?: string;
-    outDir: string;
-  }) => {
-    const { root, config, outDir } = options;
-
-    console.log(c.bold('Kubricate CLI'));
-    console.log(c.dim('A CLI for managing Kubernetes stacks'));
-    console.log(c.dim('Version: 1.0.0'));
-    console.log(c.dim('Kubricate CLI - A CLI for managing Kubernetes stacks'));
-
-    if (!config) {
-      console.log('no config file provided');
-      console.log('using default config file');
-    } else {
-      console.log(`Using config file: ${config}`);
-    }
-    console.log(`Root directory: ${root}`);
-    console.log(`Output directory: ${outDir}`);
+  .action(async (options: ApplyCommandOptions) => {
+    await new ApplyCommand(options).execute();
   })
 
 cli.version(pkg.version);
