577d9c822f26c1f107136426f5a7a53e7ec1d0d3
Thada Wangthammang <thada.wth@gmail.com>
2025-04-02 13:20:48 +0700

    misc: setup build that detect type error during lint process

--- examples/with-custom-stack/src/MyStack.ts
+++ examples/with-custom-stack/src/MyStack.ts
@@ -1,21 +1,21 @@
-import { createStack, ResourceComposer } from '@kubricate/core';
-import { Namespace } from 'kubernetes-models/v1';
+// import { createStack, ResourceComposer } from '@kubricate/core';
+// import { Namespace } from 'kubernetes-models/v1';
 
-interface MyInput {
-  name: string;
-}
+// interface MyInput {
+//   name: string;
+// }
 
-const MyStack = createStack('MyStack', (data: MyInput) => {
-  // Dosomething for your data, e.g. Validate the input data
-  return new ResourceComposer().addClass({
-    id: 'namespace',
-    type: Namespace,
-    config: {
-      metadata: { name: data.name },
-    },
-  });
-});
+// export const MyStack = createStack('MyStack', (data: MyInput) => {
+//   // Dosomething for your data, e.g. Validate the input data
+//   return new ResourceComposer().addClass({
+//     id: 'namespace',
+//     type: Namespace,
+//     config: {
+//       metadata: { name: data.name },
+//     },
+//   });
+// });
 
-export const myStack = MyStack.from({
-  name: 'my-namespace',
-});
+// export const myStack = MyStack.from({
+//   name: 'my-namespace',
+// });
