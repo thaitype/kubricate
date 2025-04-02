0a119753d4f36758471764cad2d58a6d62f06307
Thada Wangthammang <thada.wth@gmail.com>
2025-04-02 21:17:23 +0700

    test: enhance tests for joinPath and add mergeMetadata function

--- packages/toolkit/src/utils.ts
+++ packages/toolkit/src/utils.ts
@@ -5,6 +5,12 @@ export function joinPath(...paths: string[]): string {
     .join('/');
 }
 
+/**
+ * Merges metadata for Kubernetes resources.
+ *
+ * This function is may deprecated in the future. It should have better way to merge metadata.
+ * Pull Request is accepted.
+ */
 export function mergeMetadata(key: 'annotations' | 'labels', input: Record<string, string>) {
   const result: Record<string, unknown> = {};
   result[key] = input;
