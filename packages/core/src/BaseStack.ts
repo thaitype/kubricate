0398509acd8e6c4f10eeb5bb3b00aa6bafeec712
Thada Wangthammang <thada.wth@gmail.com>
2025-04-17 20:20:05 +0700

    feat: enhance secret injection strategies with targetPath support

--- packages/core/src/BaseStack.ts
+++ packages/core/src/BaseStack.ts
@@ -9,12 +9,22 @@ import {
 } from './secrets/index.js';
 import type { Objects, Call } from 'hotscript';
 
+export interface BaseSecretInjectionStrategy {
+  /**
+   * Override the default target path for the secret injection.
+   * 
+   * Moreover, each provider has a default target path for the secret injection.
+   * By using BaseProvider.getTargetPath() 
+   */
+  targetPath?: string;
+}
+
 export type SecretInjectionStrategy =
-  | { kind: 'env'; containerIndex?: number }
-  | { kind: 'volume'; mountPath: string; containerIndex?: number }
-  | { kind: 'annotation' }
-  | { kind: 'imagePullSecret' }
-  | { kind: 'envFrom'; containerIndex?: number }
+  | { kind: 'env'; containerIndex?: number } & BaseSecretInjectionStrategy
+  | { kind: 'volume'; mountPath: string; containerIndex?: number } & BaseSecretInjectionStrategy
+  | { kind: 'annotation' } & BaseSecretInjectionStrategy
+  | { kind: 'imagePullSecret' } & BaseSecretInjectionStrategy
+  | { kind: 'envFrom'; containerIndex?: number } & BaseSecretInjectionStrategy
   | { kind: 'plugin'; action?: string; args?: unknown[]; [key: string]: unknown; };
 
 export interface UseSecretsOptions<Key extends AnyKey> {
