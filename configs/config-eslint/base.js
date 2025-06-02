bd766f0df23e9413c8adfc22a3f4905b71bff52c
Thada Wangthammang <thada.wth@gmail.com>
2025-06-02 20:27:41 +0700

    misc: comment eslint plugin perfectionist/sort-imports

--- configs/config-eslint/base.js
+++ configs/config-eslint/base.js
@@ -22,40 +22,40 @@ export const config = [
       'turbo/no-undeclared-env-vars': 'warn',
     },
   },
-  {
-    plugins: {
-      perfectionist,
-    },
-    rules: {
-      'perfectionist/sort-imports': [
-        'error',
-        {
-          type: 'alphabetical',
-          order: 'asc',
-          fallbackSort: { type: 'unsorted' },
-          ignoreCase: true,
-          specialCharacters: 'keep',
-          internalPattern: ['^@kubricate/.+'],
-          partitionByComment: false,
-          partitionByNewLine: false,
-          newlinesBetween: 'always',
-          maxLineLength: undefined,
-          groups: [
-            'type-import',
-            ['value-builtin', 'value-external'],
-            'type-internal',
-            'value-internal',
-            ['type-parent', 'type-sibling', 'type-index'],
-            ['value-parent', 'value-sibling', 'value-index'],
-            'ts-equals-import',
-            'unknown',
-          ],
-          customGroups: [],
-          environment: 'node',
-        },
-      ],
-    },
-  },
+  // {
+  //   plugins: {
+  //     perfectionist,
+  //   },
+  //   rules: {
+  //     'perfectionist/sort-imports': [
+  //       'error',
+  //       {
+  //         type: 'alphabetical',
+  //         order: 'asc',
+  //         fallbackSort: { type: 'unsorted' },
+  //         ignoreCase: true,
+  //         specialCharacters: 'keep',
+  //         internalPattern: ['^@kubricate/.+'],
+  //         partitionByComment: false,
+  //         partitionByNewLine: false,
+  //         newlinesBetween: 'always',
+  //         maxLineLength: undefined,
+  //         groups: [
+  //           'type-import',
+  //           ['value-builtin', 'value-external'],
+  //           'type-internal',
+  //           'value-internal',
+  //           ['type-parent', 'type-sibling', 'type-index'],
+  //           ['value-parent', 'value-sibling', 'value-index'],
+  //           'ts-equals-import',
+  //           'unknown',
+  //         ],
+  //         customGroups: [],
+  //         environment: 'node',
+  //       },
+  //     ],
+  //   },
+  // },
   {
     plugins: {
       onlyWarn,
