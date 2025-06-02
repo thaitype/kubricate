1e5d37cecefca8f34f577204b41cb8a304cde67c
Thada Wangthammang <thada.wth@gmail.com>
2025-06-02 20:44:54 +0700

    misc: add prettier-plugin-sort-imports

---
+++ prettier.config.cjs
@@ -0,0 +1,20 @@
+// @ts-check
+
+/** @type {import("prettier").Config} */
+module.exports = {
+  // Standard prettier options
+  trailingComma: 'es5',
+  tabWidth: 2,
+  useTabs: false,
+  semi: true,
+  singleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+  // Since prettier 3.0, manually specifying plugins is required
+  plugins: ['@ianvs/prettier-plugin-sort-imports'],
+  // This plugin's options
+  importOrder: ['<BUILTIN_MODULES>', '<THIRD_PARTY_MODULES>', '', '^@kubricate/(.*)$', '', '^[./]'],
+  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
+  importOrderTypeScriptVersion: '5.0.0',
+  importOrderCaseSensitive: false,
+};
