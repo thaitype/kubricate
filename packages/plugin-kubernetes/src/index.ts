d62a8c61fa6dd80e59bd483de9d5249fe8102185
Thada Wangthammang <thada.wth@gmail.com>
2025-10-27 11:55:06 +0700

    feat: Add CustomTypeSecretProvider for flexible Kubernetes Secret management

--- packages/plugin-kubernetes/src/index.ts
+++ packages/plugin-kubernetes/src/index.ts
@@ -3,6 +3,7 @@ export * from './DockerConfigSecretProvider.js';
 export * from './BasicAuthSecretProvider.js';
 export * from './SshAuthSecretProvider.js';
 export * from './TlsSecretProvider.js';
+export * from './CustomTypeSecretProvider.js';
 export * from './utils.js';
 export * from './merge-utils.js';
 export * from './kubernetes-types.js';
